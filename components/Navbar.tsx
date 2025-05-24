814e777076a93728f208efaeb2f6f71c236b3f73
bolgerru <bolgerru@tcd.ie>
2025-05-25 00:56:59 +0300

    fixed leaderboard link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar() {
               Schedule
             </Link>
             <Link 
-              href="/results" 
+              href="/leaderboard" 
               className="px-4 py-2 text-gray-700 rounded-full hover:bg-blue-50 hover:text-blue-600 transition-colors"
             >
               Leaderboard
@@ -113,7 +113,7 @@ export default function Navbar() {
               Schedule
             </Link>
             <Link 
-              href="/results" 
+              href="/leaderboard" 
               className="px-4 py-2 text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded-lg transition-colors"
               onClick={() => setIsMenuOpen(false)}
             >
